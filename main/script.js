c7e95fc793675cd6655433ebadf31c9ee43808a4
HostVanyaD <reeedruuum@gmail.com>
2022-09-13 16:00:44 +0300

    Add basic html

---
+++ main/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// const thumbnails = document.querySelectorAll('.thumbnails-col');
+
+// let currSlide = 0;
+// const maxSlide = thumbnails.length - 1;
+
+// const expandPicture = function (thumbnailSrc) {
+//   var expandImg = document.getElementById('expandedImg');
+
+//   expandImg.src = thumbnailSrc;
+// };
+
+// expandPicture(thumbnails[0].firstChild.src);
+
+// thumbnails.forEach((thumbnail) =>
+//   thumbnail.addEventListener('click', expandPicture(thumbnail))
+// );
